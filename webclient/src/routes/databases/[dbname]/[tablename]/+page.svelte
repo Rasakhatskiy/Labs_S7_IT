f691099ffd4bcaf8687b442f779769c046e9547b
Rasakhatskyi <saharok.maks@gmail.com>
2022-10-30 22:46:36 +0200

    ...

--- webclient/src/routes/databases/[dbname]/[tablename]/+page.svelte
+++ webclient/src/routes/databases/[dbname]/[tablename]/+page.svelte
@@ -1,7 +1,53 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+
+	import { PUBLIC_API_BASE_URL } from '$env/static/public';
+	import { invalidateAll } from '$app/navigation';
+	import axios from 'axios';
+
+	let addData = [];
+	let url = `${PUBLIC_API_BASE_URL}/databases/${data.dbname}/${data.tablename}`;
+
+	function onAdd() {
+		let addUrl = `${url}/new_row`;
+		axios
+			.post(addUrl, addData)
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+
+		addData = [];
+
+		invalidateAll();
+	}
+
+	function onDelete(id) {
+		let delUrl = `${url}/${id}`;
+
+		axios
+			.delete(delUrl)
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+
+		invalidateAll();
+	}
+
+	function onEdit(id) {
+		let editUrl = `${url}/${id}`;
+
+
+	}
 </script>
+
+
 
 <!-- header -->
 <div class="sticky top-0 flex flex-col justify-center overflow-hidden bg-gray-50">
@@ -53,14 +99,16 @@
 					<th scope="col" class="px-6 py-3">
 						<span class="sr-only">Edit</span>
 					</th>
+					<th />
 				</tr>
 
 				<tr class="bg-gray-700 border-t-2 border-gray-600">
-					{#each data.table.headers as header}
+					{#each data.table.headers as header, i}
 						<th scope="col" class="px-6 py-3">
 							<input
 								type="text"
 								id="last_name"
+								bind:value={addData[i]}
 								class="text-xs rounded-lg   block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-500 text-white outline-none"
 								placeholder={header.name}
 								required
@@ -68,12 +116,17 @@
 						</th>
 					{/each}
 					<td class="px-6 py-4 text-right">
-						<a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Add</a>
+						<a
+							href="#"
+							class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+							on:click={onAdd}>Add</a
+						>
 					</td>
+					<td />
 				</tr>
 			</thead>
 			<tbody>
-				{#each data.table.values as row}
+				{#each data.table.values as row, i}
 					<tr
 						class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
 					>
@@ -83,6 +136,13 @@
 						<td class="px-6 py-4 text-right">
 							<a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
 								>Edit</a
+							>
+						</td>
+						<td class="px-6 py-4 text-right">
+							<a
+								href="#"
+								class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+								on:click|preventDefault={() => onDelete(i)}>Delete</a
 							>
 						</td>
 					</tr>
