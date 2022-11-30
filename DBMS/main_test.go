3c37d50673c1221d576b129bf364a951d9599bd1
Rasakhatskyi <saharok.maks@gmail.com>
2022-12-01 01:12:12 +0200

    tests

--- DBMS/main_test.go
+++ DBMS/main_test.go
@@ -5,17 +5,71 @@ import (
 	"testing"
 )
 
-func createTestDB() *database.Database {
-	return &database.Database{
-		Name: "Name",
-		Tables: []database.Table{
-			{
-				Name:    "",
-				Types:   nil,
-				Headers: nil,
-				Values:  nil,
-			},
+const (
+	testDBName = "test"
+)
+
+func TestCreateDatabase(t *testing.T) {
+	err := database.CreateDatabase(testDBName)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestReadDatabasePaths(t *testing.T) {
+	result := database.ReadDatabasesPaths()
+
+	for _, json := range result {
+		if json.Name == testDBName {
+			return
+		}
+	}
+
+	t.Fatal("DB not found")
+}
+
+func TestLoadDatabase(t *testing.T) {
+	db, err := database.LoadDatabase(testDBName)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if db.Name != testDBName {
+		t.Fatal("loaded wrong db")
+	}
+}
+
+func TestAddTable(t *testing.T) {
+	db, err := database.LoadDatabase(testDBName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	table := database.Table{
+		Name: "dogs",
+		Types: []string{
+			database.TypeIntegerTS,
+			database.TypeStringTS,
+			database.TypeIntegerTS,
+			database.TypeStringRangeTS,
+			database.TypeHTMLTS,
 		},
+		Headers: []string{
+			"id",
+			"name",
+			"owner_id",
+			"range",
+			"page",
+		},
+		Values: nil,
+	}
+
+	err = db.AddTable(table)
+
+	if err != nil {
+		t.Fatal(err.Error())
 	}
 }
 
