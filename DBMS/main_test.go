a4feb56d156a754b3d910ff2e5d8097203879b16
Rasakhatskyi <saharok.maks@gmail.com>
2022-12-02 00:29:12 +0200

    tests

--- DBMS/main_test.go
+++ DBMS/main_test.go
@@ -9,6 +9,55 @@ const (
 	testDBName = "test"
 )
 
+var testTable1 = database.Table{
+	Name: "dogs",
+	Types: []string{
+		database.TypeIntegerTS,
+		database.TypeStringTS,
+		database.TypeIntegerTS,
+		database.TypeStringRangeTS,
+		database.TypeHTMLTS,
+	},
+	Headers: []string{
+		"id",
+		"name",
+		"owner_id",
+		"range",
+		"page",
+	},
+	Values: nil,
+}
+
+var testTable2 = database.Table{
+	Name: "owners",
+	Types: []string{
+		database.TypeIntegerTS,
+		database.TypeStringTS,
+	},
+	Headers: []string{
+		"id",
+		"name",
+	},
+	Values: nil,
+}
+
+var testValues1 = [][]database.DBType{
+	{
+		database.TypeInteger{Val: 1},
+		database.TypeString{Val: "bobrik"},
+		database.TypeInteger{Val: 2},
+		database.TypeStringRange{Val: []string{"aaaa", "bbbb"}},
+		database.TypeHTML{Val: "<div> a </div>"},
+	},
+}
+
+var testValues2 = [][]database.DBType{
+	{
+		database.TypeInteger{Val: 2},
+		database.TypeString{Val: "Oleh"},
+	},
+}
+
 func TestCreateDatabase(t *testing.T) {
 	err := database.CreateDatabase(testDBName)
 
@@ -47,26 +96,7 @@ func TestAddTable(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 
-	table := database.Table{
-		Name: "dogs",
-		Types: []string{
-			database.TypeIntegerTS,
-			database.TypeStringTS,
-			database.TypeIntegerTS,
-			database.TypeStringRangeTS,
-			database.TypeHTMLTS,
-		},
-		Headers: []string{
-			"id",
-			"name",
-			"owner_id",
-			"range",
-			"page",
-		},
-		Values: nil,
-	}
-
-	err = db.AddTable(table)
+	err = db.AddTable(testTable1)
 
 	if err != nil {
 		t.Fatal(err.Error())
@@ -75,6 +105,11 @@ func TestAddTable(t *testing.T) {
 
 func TestGetTable(t *testing.T) {
 	db, err := database.LoadDatabase(testDBName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	err = db.AddTable(testTable1)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
@@ -89,24 +124,61 @@ func TestGetTable(t *testing.T) {
 	}
 }
 
-func TestAddRow(t *testing.T) {
-	db, err := database.LoadDatabase(testDBName)
+func TestJoinTables(t *testing.T) {
+	testTable1.Values = testValues1
+	testTable2.Values = testValues2
+
+	joined, err := database.JoinTables(testTable1, testTable2, "owner_id", "id")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
-	table, err := db.GetTable("dogs")
-	if err != nil {
-		t.Fatal(err.Error())
+	if len(joined.Values) != 1 || len(joined.Values[0]) != 6 {
+		t.Fatalf("wrong joined size")
 	}
 
-	table.AddRecord("")
+	for i := 0; i < 5; i++ {
+		if i == 3 {
+			continue
+		}
+		if joined.Values[0][i].Value() != testValues1[0][i].Value() {
+			t.Fatalf("wrong joined result %d", i)
+		}
+	}
+
+	if joined.Values[0][5].Value() != testValues2[0][1].Value() {
+		t.Fatalf("wrong joined result 5")
+	}
 }
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestHelloName(t *testing.T) {
-	if true {
-		t.Fatalf("E")
+func TestValidateStringRange_1(t *testing.T) {
+	data := database.TypeStringRange{}
+	err := data.Validate("arbol", "bruh momento")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestValidateStringRange_2(t *testing.T) {
+	data := database.TypeStringRange{}
+	err := data.Validate("bober", "bebra")
+	if err == nil {
+		t.Fatal("invalid validation")
+	}
+}
+
+func TestValidateHTML_1(t *testing.T) {
+	data := database.TypeHTML{}
+	err := data.Validate("<div> <bruh> <momento> numero 2 </momento> </bruh> </div>")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestValidateHTML_2(t *testing.T) {
+	data := database.TypeHTML{}
+	err := data.Validate("<div> <bruh> <momento> numero 2 </momento> </div>")
+	if err == nil {
+		t.Fatal("invalid validation")
 	}
 }
